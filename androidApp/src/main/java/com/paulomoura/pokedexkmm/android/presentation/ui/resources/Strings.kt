61fbc92910c44233a43a8da29e19abe1164154bc
Moura <paulovenicius.demoura@wexinc.com>
2023-04-22 22:50:53 -0300

    search bar ios

--- androidApp/src/main/java/com/paulomoura/pokedexkmm/android/presentation/ui/resources/Strings.kt
+++ androidApp/src/main/java/com/paulomoura/pokedexkmm/android/presentation/ui/resources/Strings.kt
@@ -1,6 +1,6 @@
 package com.paulomoura.pokedexkmm.android.presentation.ui.resources
 
-const val APP_NAME = "Pokedéx"
+const val APP_NAME = "Pokédex"
 const val TYPE = "Type"
 const val EVOLUTIONS = "Evolutions"
 const val NO_EVOLUTION = "This Pokémon does not evolve."
--- iosApp/iosApp/pokemonlist/ListPokemon.swift
+++ iosApp/iosApp/pokemonlist/ListPokemon.swift
@@ -3,23 +3,35 @@ import shared
 
 struct ListPokemon: View {
     
+    @State private var searchText = ""
     private let pokemons: [PokemonListItem]
+    private var filteredPokemons: [PokemonListItem] {
+        if (searchText.isEmpty) {
+            return pokemons
+        } else {
+            return pokemons.filter{ $0.doesMatchSearch(query: searchText) }
+        }
+    }
     
     init(pokemons: [PokemonListItem]) {
         self.pokemons = pokemons
     }
     
     var body: some View {
-        List(pokemons, id: \.number) { pokemon in
-            HStack {
-                AsyncImage(url: URL(string: pokemon.imageUrl), content: { image in
-                    image.resizable().aspectRatio(contentMode: .fit)
-                }, placeholder: {
-                    Image(systemName: "photo.fill")
-                })
-                Text(String(pokemon.number)).padding().font(.title3)
-                Text(pokemon.name).font(.title3)
-            }.frame(width: nil, height: 70)
+        NavigationView {
+            List(filteredPokemons, id: \.number) { pokemon in
+                HStack {
+                    AsyncImage(url: URL(string: pokemon.imageUrl), content: { image in
+                        image.resizable().aspectRatio(contentMode: .fit)
+                    }, placeholder: {
+                        Image(systemName: "photo.fill")
+                    })
+                    Text(String(format: "%03d", pokemon.number)).padding().font(.title3)
+                    Text(pokemon.name).font(.title3)
+                }.frame(width: nil, height: 70)
+            }
+            .searchable(text: $searchText)
+            .navigationTitle("Pokédex")
         }
     }
 }
