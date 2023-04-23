66a9bcf3086d22529f52a0216ed57cc116f1b3fb
Moura <paulovenicius.demoura@wexinc.com>
2023-04-23 13:37:32 -0300

    replacing rxSwift for combine

--- iosApp/iosApp/pokemondetail/PokemonDetailViewModel.swift
+++ iosApp/iosApp/pokemondetail/PokemonDetailViewModel.swift
@@ -1,10 +1,11 @@
 import shared
-import RxSwift
-import KMPNativeCoroutinesRxSwift
+import Combine
+import KMPNativeCoroutinesCombine
 
 class PokemonDetailViewModel : ObservableObject {
     
     @Published var uiState = PokemonDetailState.loading(false)
+    private var cancellable: AnyCancellable? = nil
     
     init(pokemonNumber: Int32) {
         getPokemon(pokemonNumber: pokemonNumber)
@@ -12,30 +13,24 @@ class PokemonDetailViewModel : ObservableObject {
     
     private func getPokemon(pokemonNumber: Int32) {
         let useCase = UseCasesHelper().getPokemonUseCase
-        let observable = createObservable(for: useCase.getPokemon(number: pokemonNumber))
-        let _ = observable
-            .observe(on: MainScheduler.instance)
-            .subscribe(onNext: { value in
-                switch(value) {
-                case is ResponseLoading<Pokemon>:
-                    self.uiState = PokemonDetailState.loading(true)
-                case is ResponseSuccess<Pokemon>:
-                    if let pokemon = value.data {
-                        self.uiState = PokemonDetailState.pokemon(pokemon)
-                    } else {
-                        self.uiState = PokemonDetailState.error(ErrorConstants().ERROR_POKEMON_DETAIL_STATE)
-                    }
-                case is ResponseError<Pokemon>:
+        let publisher = createPublisher(for: useCase.getPokemon(number: pokemonNumber))
+        cancellable = publisher.receive(on: DispatchQueue.main).sink { completion in
+            print("Received completion: \(completion)")
+        } receiveValue: { value in
+            switch(value) {
+            case is ResponseLoading<Pokemon>:
+                self.uiState = PokemonDetailState.loading(true)
+            case is ResponseSuccess<Pokemon>:
+                if let pokemon = value.data {
+                    self.uiState = PokemonDetailState.pokemon(pokemon)
+                } else {
                     self.uiState = PokemonDetailState.error(ErrorConstants().ERROR_POKEMON_DETAIL_STATE)
-                default:
-                    break
                 }
-            }, onError: { error in
-                print("Received error: \(error)")
-            }, onCompleted: {
-                print("Observable completed")
-            }, onDisposed: {
-                print("Observable disposed")
-            })
+            case is ResponseError<Pokemon>:
+                self.uiState = PokemonDetailState.error(ErrorConstants().ERROR_POKEMON_DETAIL_STATE)
+            default:
+                break
+            }
+        }
     }
 }
--- iosApp/iosApp/pokemonlist/PokemonListViewModel.swift
+++ iosApp/iosApp/pokemonlist/PokemonListViewModel.swift
@@ -1,10 +1,11 @@
 import shared
-import RxSwift
-import KMPNativeCoroutinesRxSwift
+import Combine
+import KMPNativeCoroutinesCombine
 
 class PokemonListViewModel : ObservableObject {
     
     @Published var uiState = PokemonListState.loading(false)
+    private var cancellable: AnyCancellable? = nil
     
     init() {
         getPokemons()
@@ -12,26 +13,20 @@ class PokemonListViewModel : ObservableObject {
     
     private func getPokemons() {
         let useCase = UseCasesHelper().getPokemonsUseCase
-        let observable = createObservable(for: useCase.getPokemons())
-        let _ = observable
-            .observe(on: MainScheduler.instance)
-            .subscribe(onNext: { value in
-                switch(value) {
-                case is ResponseLoading<NSArray>:
-                    self.uiState = PokemonListState.loading(true)
-                case is ResponseSuccess<NSArray>:
-                    self.uiState = PokemonListState.pokemonListItems(value.data as! [PokemonListItem])
-                case is ResponseError<NSArray>:
-                    self.uiState = PokemonListState.error(ErrorConstants().ERROR_POKEMON_LIST_STATE)
-                default:
-                    break
-                }
-            }, onError: { error in
-                print("Received error: \(error)")
-            }, onCompleted: {
-                print("Observable completed")
-            }, onDisposed: {
-                print("Observable disposed")
-            })
+        let publisher = createPublisher(for: useCase.getPokemons())
+        cancellable = publisher.receive(on: DispatchQueue.main).sink { completion in
+            print("Received completion: \(completion)")
+        } receiveValue: { value in
+            switch(value) {
+            case is ResponseLoading<NSArray>:
+                self.uiState = PokemonListState.loading(true)
+            case is ResponseSuccess<NSArray>:
+                self.uiState = PokemonListState.pokemonListItems(value.data as? [PokemonListItem] ?? [])
+            case is ResponseError<NSArray>:
+                self.uiState = PokemonListState.error(ErrorConstants().ERROR_POKEMON_LIST_STATE)
+            default:
+                break
+            }
+        }
     }
 }
