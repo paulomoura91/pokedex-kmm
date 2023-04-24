04124641047712d8f6d0871d39b3e9575655d26c
Moura <paulovenicius.demoura@wexinc.com>
2023-04-24 19:22:00 -0300

    evolutions added to detail

--- iosApp/iosApp/pokemondetail/DetailPokemonView.swift
+++ iosApp/iosApp/pokemondetail/DetailPokemonView.swift
@@ -6,47 +6,60 @@ struct DetailPokemonView: View {
     let pokemon: Pokemon
     
     var body: some View {
-        VStack {
-            Text(pokemon.name).font(.title)
-            Text("Nº \(String(format: "%03d", pokemon.number))" ).font(.title2)
-            AsyncImage(url: URL(string: pokemon.imageUrl), content: { image in
-                image
-                    .resizable()
-                    .frame(width: 250, height: 250)
-            }, placeholder: {
-                Image(systemName: "photo.fill")
-            })
-            Text(pokemon.description_)
+        ScrollView {
+            VStack {
+                Text(pokemon.name).font(.title)
+                Text("Nº \(String(format: "%03d", pokemon.number))" ).font(.title2)
+                AsyncImage(url: URL(string: pokemon.imageUrl), content: { image in
+                    image
+                        .resizable()
+                        .frame(width: 250, height: 250)
+                }, placeholder: {
+                    Image(systemName: "photo.fill")
+                })
+                Text(pokemon.description_)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.bottom, 15)
+                Text("Type")
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .font(.title2)
+                HStack {
+                    ForEach(pokemon.types, id: \.self) { type in
+                        Text(type.uppercased())
+                            .frame(width: 120)
+                            .padding(10)
+                            .font(.system(size: 15))
+                            .background(getTypeColor(type: type))
+                            .cornerRadius(40)
+                            .padding(5)
+                    }
+                }
                 .frame(maxWidth: .infinity, alignment: .leading)
                 .padding(.bottom, 15)
-            Text("Type")
-                .frame(maxWidth: .infinity, alignment: .leading)
-                .font(.title2)
-            HStack {
-                ForEach((pokemon.types), id: \.self) { type in
-                    Text(type.uppercased())
-                        .frame(width: 120)
-                        .padding(10)
-                        .font(.system(size: 15))
-                        .background(getTypeColor(type: type))
-                        .cornerRadius(40)
-                        .padding(5)
+                Text("Evolutions")
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .font(.title2)
+                if (pokemon.evolutions.count == 1) {
+                    Text("This Pokémon does not evolve.")
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .padding(.top, 10)
+                        .padding(.bottom, 10)
+                }
+                ForEach(pokemon.evolutions, id: \.number) { evolution in
+                    AsyncImage(url: URL(string: evolution.imageUrl), content: { image in
+                        image
+                            .resizable()
+                            .frame(width: 115, height: 115)
+                    }, placeholder: {
+                        Image(systemName: "photo.fill")
+                    })
+                    Text(evolution.name)//.font(.title)
+                    Text("Nº \(String(format: "%03d", evolution.number))" )//.font(.title2)
                 }
             }
-            .frame(maxWidth: .infinity, alignment: .leading)
-            .padding(.bottom, 15)
-            Text("Evolutions")
-                .frame(maxWidth: .infinity, alignment: .leading)
-                .font(.title2)
-            if (pokemon.evolutions.count == 1) {
-                Text("This Pokémon does not evolve.")
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                    .padding(.top, 10)
-                    .padding(.bottom, 10)
-            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
+            .padding(.all, 10)
         }
-        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
-        .padding(.all, 10)
     }
     
     private func getTypeColor(type: String) -> Color {
